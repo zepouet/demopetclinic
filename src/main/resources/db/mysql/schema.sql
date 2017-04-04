f1f188d69b53e06f16666fa6e0c196ccf7a1c87d
Nicolas <zepouet@gmail.com>
2017-04-04 13:42:25 +0200

    Update

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
-
-ALTER DATABASE petclinic
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
-
-GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
-
 USE petclinic;
 
 CREATE TABLE IF NOT EXISTS vets (
